bf86fdfdbe3c9a3b47639c8b38998179d35ae30c
GOTWIC <swagnik17@gmail.com>
2023-07-18 01:56:33 -0400

    Added List Scene Prototype

--- Scripts/root.gd
+++ Scripts/root.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 var MapRenderer = preload("res://Subscenes/MapRenderer.tscn")
 var GraphView = preload("res://Subscenes/GraphScene.tscn")
-var ListView
+var ListView = preload("res://Subscenes/ListScene.tscn")
 
 var MapLoadingScene = preload("res://Subscenes/MapLoadingScene.tscn")
 
@@ -10,11 +10,13 @@ var MapLoadingScene = preload("res://Subscenes/MapLoadingScene.tscn")
 func _ready():
 	var mapRendererInst = MapRenderer.instantiate()
 	var graphViewInst = GraphView.instantiate()
+	var listViewInst = ListView.instantiate()
 	
 	var mapLoadingSceneInst = MapLoadingScene.instantiate()
 	
 	get_node("MapView").add_child(mapRendererInst)
 	get_node("GraphView").add_child(graphViewInst)
+	get_node("ListView").add_child(listViewInst)
 	
 	get_node("MapView").add_child(mapLoadingSceneInst)
 
